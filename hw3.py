d9a2e44ab40030a330e9121f6bf2b34eea4bec8e
nhitch <113487798+nhitch@users.noreply.github.com>
2022-09-28 22:32:51 -0400

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -3,14 +3,14 @@ from flask import request, redirect, session
 import os
 import psycopg2
 
-hw3 = Flask(__name__)
+app = Flask(__name__)
 conn = psycopg2.connect(
         host="db-hw3.co9ywatzeczd.us-east-2.rds.amazonaws.com",
         database="postgres",
         user="postgres",
         password="sickntired")
 
-@hw3.route('/')
+@app.route('/')
 def main():
     conn = psycopg2.connect(
     host="db-hw3.co9ywatzeczd.us-east-2.rds.amazonaws.com",
@@ -25,4 +25,4 @@ def main():
     return render_template('hw3.html', data = stringA)
 
 if(__name__ == "__main__"):
-    hw3.run()
+    app.run()
